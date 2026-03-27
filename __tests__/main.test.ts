91da30f2411ea1691fb23a493e7a28104b5d89b2
Anton Winter <anton@ketch.com>
2026-03-26 21:38:29 -0700

    feat!: Migrate to ESM, esbuild, and Node 24
    BREAKING CHANGE: Minimum runner must support node24.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,9 +1,12 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import {fileURLToPath} from 'url'
 import {test} from '@jest/globals'
 import os from 'os'
 import * as fs from 'fs'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
@@ -12,7 +15,7 @@ test('test runs', () => {
   process.env['INPUT_PASSWORD'] = 'pass'
 
   const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const ip = path.join(__dirname, '..', 'dist', 'index.cjs')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
